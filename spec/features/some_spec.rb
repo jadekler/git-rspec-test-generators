a9209f45092b3d36aa7496b7e457133fdd46cabd
Jean de Klerk <jadekler@gmail.com>
2015-12-09 22:44:26 -0700

    Initial commit

---
+++ spec/features/some_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../spec_helper'
+
+describe 'something' do
+  it 'works' do
+    expect(1).to eq 1
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,3 @@
+RSpec.configure do |config|
+  config.default_formatter = 'doc'
+end
